e6e66c091b21bf8183265d36bbc8466d00f5e0f9
Shayne Evans <evansshayne3@gmail.com>
2024-11-12 14:35:00 -0500

    Applying .gitignore to existing tracked files

---
+++ BetterSnippingTool/AppConfig.cs
@@ -0,0 +1,107 @@
+﻿//Configuration file used to load default values or other user created profiles.
+
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace BetterSnippingTool.Config
+{
+    public class AppConfig
+    {
+        public string DefaultDirectory { get; set; } = string.Empty;
+        public enum OptimizeGifCreation
+        {
+            Optimize = 0,   //Optimizes gif creation by scaling down aspet ratio
+            SnippedRes = 1,      //Use snipped resolution
+            CustomRes = 2    //Choose from set of custom resolutions
+        }
+
+        public OptimizeGifCreation OptimizationLevel { get; set; } = OptimizeGifCreation.Optimize;
+        public (int, int) gifOutputResolution { get; set; }
+        public string gifDirectory { get; set; }
+        private int fps = 24;
+        public int FPS
+        {
+            get => fps;
+            set => fps = value >= 1 && value <= 60
+                ? value
+                : 24;
+        }
+        private int seconds = 5;
+        public int Seconds
+        {
+            get => seconds;
+            set => seconds = value >= 1 && value <= 20
+                ? value
+                : 5;
+        }
+
+        private static AppConfig instance;
+        private string currentFileName;
+
+        public static AppConfig Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = LoadConfig("default.xml");
+                }
+                return instance;
+            }
+        }
+
+        private static string GetFilePath(string fileName)
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string parentDirectory = Directory.GetParent(baseDirectory).FullName;
+            string profilesPath = Path.Combine(parentDirectory, "Profiles");
+            return Path.Combine(profilesPath, $"{fileName}");
+        }
+
+        private static AppConfig LoadConfig(string configName)
+        {
+            string filePath = GetFilePath(configName);
+
+            if (File.Exists(filePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(AppConfig));
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    instance = (AppConfig)serializer.Deserialize(reader);
+                    instance.currentFileName = configName;
+                }
+            }
+            else
+            {
+                instance = new AppConfig();
+                instance.SaveConfig(configName);
+            }
+            return instance;
+        }
+
+        public void SaveConfig(string fileName)
+        {
+            string filePath = GetFilePath(fileName);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(AppConfig));
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                serializer.Serialize(writer, this);
+            }
+
+            currentFileName = fileName;
+        }
+
+        public void SwitchToConfig(string newFileName)
+        {
+            LoadConfig(newFileName);
+        }
+
+        public string GetCurrentFileName()
+        {
+            return currentFileName;
+        }
+    }
+}
---
+++ BetterSnippingTool/FileUtilities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetterSnippingTool.Utilities
+{
+    public class FileUtilities
+    {
+        public string executableDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        public string outputDir;
+        public string tempDir;
+
+        public FileUtilities() 
+        {
+            this.outputDir = Path.Combine(executableDirectory, "GIF");
+            this.tempDir = Path.Combine(outputDir, "GIF_Temp_Screenshots");
+            Directory.CreateDirectory(outputDir);
+            Directory.CreateDirectory(tempDir);
+        }
+
+        //Clears temp folder of all temp files
+        public void ClearTemp()
+        {
+            try
+            {
+                if (Directory.Exists(this.tempDir))
+                {
+                    Array.ForEach(Directory.GetFiles(this.tempDir), File.Delete);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error clearing temp directory: {ex.Message}");
+            }
+        }
+
+        //Deletes gif file
+        public void ClearGifFolder(string gifFilePath)
+        {
+            //Extract the directory path up to the GIF folder
+            string gifFolderPath = Path.GetDirectoryName(gifFilePath);
+            Array.ForEach(Directory.GetFiles(gifFolderPath), File.Delete);
+        }
+    }
+}
