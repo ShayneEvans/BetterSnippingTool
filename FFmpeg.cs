3bad5c780257cf171f04f63715a15e988ca55730
Shayne Evans <evansshayne3@gmail.com>
2024-11-12 15:27:26 -0500

    Reinitialize Git Repository

---
+++ FFmpeg.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BetterSnippingTool.Tools
+{
+    public static class FFmpeg
+    {
+        public static void run_command(string ffmpegPath, string command)
+        {
+            using (Process p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = ffmpegPath;
+                p.StartInfo.Arguments = command;
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+    }
+}
